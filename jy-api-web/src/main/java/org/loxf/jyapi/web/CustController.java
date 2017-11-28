9bf14c08a52b19837861c57beb5bca032b84e1e3
luohj <myself35335@163.com>
2017-11-28 17:51:26 +0800

    绑定用户接口

--- jy-api-web/src/main/java/org/loxf/jyapi/web/CustController.java
+++ jy-api-web/src/main/java/org/loxf/jyapi/web/CustController.java
@@ -14,6 +14,7 @@ import org.loxf.jyadmin.client.dto.AccountDto;
 import org.loxf.jyadmin.client.dto.CustDto;
 import org.loxf.jyadmin.client.service.AccountService;
 import org.loxf.jyadmin.client.service.CustService;
+import org.loxf.jyadmin.client.service.VerifyCodeService;
 import org.loxf.jyapi.util.CookieUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -38,13 +39,17 @@ public class CustController {
     private CustService custService;
     @Autowired
     private AccountService accountService;
+    @Autowired
+    private VerifyCodeService verifyCodeService;
+
     /**
      * 初始化接口
+     *
      * @return
      */
     @RequestMapping("/api/cust/init")
     @ResponseBody
-    public BaseResult init(HttpServletRequest request){
+    public BaseResult init(HttpServletRequest request) {
         CustDto custDto = CookieUtil.getCust(request);
         BaseResult<JSONObject> accountDtoBaseResult = accountService.queryAccount(custDto.getCustId());
         JSONObject jsonObject = accountDtoBaseResult.getData();
@@ -58,68 +63,100 @@ public class CustController {
 
     /**
      * 我的同学
+     *
      * @param type 1:直接同学 2：间接同学
      * @return
      */
     @RequestMapping("/api/cust/myClassmate")
     @ResponseBody
-    public BaseResult myClassmate(Integer page, Integer size, Integer type){
+    public BaseResult myClassmate(Integer page, Integer size, Integer type) {
         return new BaseResult();
     }
+
     /**
      * 我的活动
+     *
      * @return
      */
     @RequestMapping("/api/cust/activity")
     @ResponseBody
-    public BaseResult myActivity(Integer page, Integer size){
+    public BaseResult myActivity(Integer page, Integer size) {
         return new BaseResult();
     }
+
     /**
      * 我的订单
+     *
      * @return
      */
     @RequestMapping("/api/cust/myorder")
     @ResponseBody
-    public BaseResult myorder(){
+    public BaseResult myorder() {
         return new BaseResult();
     }
+
     /**
      * 积分排行榜前十列表
+     *
      * @return
      */
     @RequestMapping("/api/bp/rankingList")
     @ResponseBody
-    public BaseResult rankingList(){
+    public BaseResult rankingList() {
         return new BaseResult();
     }
+
     /**
      * 积分明细
+     *
      * @param type 0:全部 1:收入 3:支出
      * @return
      */
     @RequestMapping("/api/cust/bpDetail")
     @ResponseBody
-    public BaseResult bpDetail(Integer page, Integer size, Integer type){
+    public BaseResult bpDetail(Integer page, Integer size, Integer type) {
         return new BaseResult();
     }
+
     /**
      * 绑定手机
+     *
      * @param isChinese 1:国内用户 2:海外用户
      * @return
      */
     @RequestMapping("/api/cust/bindPhone")
     @ResponseBody
-    public BaseResult bindPhone(String email, String phone, Integer isChinese, String verify){
-        return new BaseResult();
+    public BaseResult bindPhone(HttpServletRequest request, String email, String phone, Integer isChinese, String verifyCode) {
+        CustDto custDto = CookieUtil.getCust(request);
+        if(custDto.getIsChinese()==null) {
+            String target ;
+            if(isChinese==1){
+                target = phone;
+            } else {
+                target = email;
+            }
+            BaseResult verifyResult = verifyCodeService.verify(custDto.getCustId(), verifyCode);
+            if(verifyResult.getCode()==BaseConstant.SUCCESS) {
+                custDto.setIsChinese(isChinese);
+                custDto.setEmail(email);
+                custDto.setPhone(phone);
+                custService.updateCust(custDto);
+                return new BaseResult();
+            } else {
+                return new BaseResult(BaseConstant.FAILED, "验证码错误");
+            }
+        }
+        return new BaseResult(BaseConstant.FAILED, "用户已绑定，不能重复绑定");
     }
+
     /**
      * 观看记录
+     *
      * @return
      */
     @RequestMapping("/api/cust/watchRecordList")
     @ResponseBody
-    public BaseResult watchRecordList(Integer page, Integer size){
+    public BaseResult watchRecordList(Integer page, Integer size) {
         return new BaseResult();
     }
 }
