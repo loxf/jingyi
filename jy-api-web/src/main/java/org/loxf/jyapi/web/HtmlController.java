a7837a483ddc63a754d6f3ae72325e0012172732
luohj <myself35335@163.com>
2017-12-02 16:17:09 +0800

    富文本接口

--- jy-api-web/src/main/java/org/loxf/jyapi/web/HtmlController.java
+++ jy-api-web/src/main/java/org/loxf/jyapi/web/HtmlController.java
@@ -1,21 +1,26 @@
 package org.loxf.jyapi.web;
 
 import org.loxf.jyadmin.base.bean.BaseResult;
+import org.loxf.jyadmin.client.service.HtmlInfoService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HtmlController {
+    @Autowired
+    private HtmlInfoService htmlInfoService;
 
     /**
      * 获取富文本内容
+     *
      * @param htmlId 富文本ID
      * @return
      */
     @RequestMapping("/api/html/getHtml")
     @ResponseBody
-    public BaseResult getHtml(String htmlId){
-        return new BaseResult();
+    public BaseResult getHtml(String htmlId) {
+        return htmlInfoService.getHtml(htmlId);
     }
 }
